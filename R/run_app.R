8ee37c05215a4449fc89edd27e50dce8c81320a0
Ivan Plyushchenko, PhD <plyush1993@bk.ru>
2026-03-19 22:54:32 +0200

    Add files via upload

--- R/run_app.R
+++ R/run_app.R
@@ -1,9 +1,28 @@
 #' Run the qry4ms Application
-#' Online version: https://plyush1993.shinyapps.io/qry4ms/
 #'
+#' @importFrom crayon blue red bold
 #' @export
 run_qry4ms <- function(...) {
-  options(shiny.maxRequestSize = 1 * 1024^3)
+
+  cat("\n")
+  cat(crayon::blue("             +--------------------+\n"))
+  app_name <- paste0(
+    crayon::blue(crayon::bold("      qry4ms      "))
+  )
+  cat(crayon::blue("             | "), app_name, crayon::blue(" |\n"), sep = "")
+  cat(crayon::blue("             +--------------------+\n"))
+  cat("\n")
+  cat(crayon::red(crayon::bold("Making an MS query and basic MS-related calculations\n")))
+  cat("\n")
+
+  flush.console()
+
+  old_opts <- options(shiny.maxRequestSize = 5 * 1024^3)
+    on.exit({
+    options(old_opts)
+    gc()
+    }, add = TRUE)
+
   shiny::addResourcePath("www", system.file("www", package = "qry4ms"))
   shiny::shinyApp(ui = app_ui(), server = app_server, ...)
 }
