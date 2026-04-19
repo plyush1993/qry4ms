ae83f116afdbbd8a11ded88ac8d95c8fc9fa922f
Ivan Plyushchenko, PhD <plyush1993@bk.ru>
2026-04-19 18:10:39 +0300

    Add files via upload

--- R/run_app.R
+++ R/run_app.R
@@ -18,11 +18,14 @@ run_qry4ms <- function(...) {
   flush.console()
 
   old_opts <- options(shiny.maxRequestSize = 1 * 1024^3)
+
     on.exit({
     options(old_opts)
     gc()
     }, add = TRUE)
 
   shiny::addResourcePath("www", system.file("www", package = "qry4ms"))
-  shiny::shinyApp(ui = app_ui(), server = app_server, ...)
+
+  app <- shiny::shinyApp(ui = app_ui(), server = app_server)
+  shiny::runApp(app, ...)
 }
